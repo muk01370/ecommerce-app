046aabc279efe4ad98b3201c6692bbaae3a98cd9
muk01370 <mukesh01370@gmail.com>
2025-01-07 18:27:06 +0530

    Update index.jsx

--- admin/src/pages/ProductList/index.jsx
+++ admin/src/pages/ProductList/index.jsx
@@ -26,7 +26,7 @@ const ProductList = ({ isDarkmode }) => {
   // Fetch products
   const fetchProducts = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/products");
+      const response = await fetch("https://ecommerce-app-backend-v534.onrender.com");
       if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
       }
